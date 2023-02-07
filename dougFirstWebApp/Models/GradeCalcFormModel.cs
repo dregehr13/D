8607204924db32c03bd7d783cf2bcb0708217c72
Douglas Regehr <regehrdoug@gmail.com>
2023-02-06 22:54:22 -0700

    updated to mvc

---
+++ dougFirstWebApp/Models/GradeCalcFormModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dougFirstWebApp.Models
+{
+    public class GradeCalcFormModel
+    {
+        [Range(0, 100)]
+        public int Assignments { get; set; }
+
+        [Range(0, 100)]
+        public int Project { get; set; }
+
+        [Range(0, 100)]
+        public int Quizzes { get; set; }
+
+        [Range(0, 100)]
+        public int Midterm { get; set; }
+
+        [Range(0,100)]
+        public int Final { get; set; }
+
+        [Range(0, 100)]
+
+        public int Intex { get; set; }
+    }
+}
